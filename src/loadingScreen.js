8e6506d71a457bb5f2271a757be2f4f904294090
sturgis <sturgis.steele@outlook.com>
2024-12-15 21:12:16 -0600

    run npm build

--- src/loadingScreen.js
+++ src/loadingScreen.js
@@ -1,13 +1,16 @@
-import './css/loadingScreen.css';
-import { enableCamera, disableCamera } from './cameraControls.js';
+import "./css/loadingScreen.css";
+import { enableCamera, disableCamera } from "./cameraControls.js";
 
 // Start screen
 export function initializeStartScreen(onStartCallback) {
-  const startScreen = document.createElement('div');
-  startScreen.id = 'start-screen'; // Style this in your CSS
+  const startScreen = document.createElement("div");
+  startScreen.id = "start-screen"; // Style this in your CSS
   // Add the main content for the start screen
   startScreen.innerHTML = `
     <a class="button" href="#">START MATCH</a>
+    <div class="disclaimer">
+      <p>This website is entirely fan made and has no association with the deployer of $MLG cryptocurrency on the solana blockchain or any game studio. $MLG is a crypto asset with no intrinsic value or expectation of financial return. These 360noscope420blazeit coins are to be used for entertainment purposes only.</p>
+    </div>
   `;
 
   // Create 3D border elements dynamically
@@ -27,12 +30,12 @@ export function initializeStartScreen(onStartCallback) {
   document.body.appendChild(startScreen);
 
   // Attach event listener to the button
-  const startButton = startScreen.querySelector('.button');
-  startButton.addEventListener('click', (event) => {
+  const startButton = startScreen.querySelector(".button");
+  startButton.addEventListener("click", (event) => {
     event.preventDefault(); // Prevent default anchor behavior
 
     // Add the 'pressed' class to simulate button press
-    startButton.classList.add('pressed');
+    startButton.classList.add("pressed");
 
     // Delay hiding the start screen and showing the loading screen
     setTimeout(() => {
@@ -53,19 +56,19 @@ export function initializeStartScreen(onStartCallback) {
 }
 
 export function showStartScreen(startScreen) {
-  startScreen.style.display = 'flex'; // Show the start screen
+  startScreen.style.display = "flex"; // Show the start screen
   disableCamera(); // Disable camera movement
 }
 
 export function hideStartScreen(startScreen) {
-  startScreen.style.display = 'none'; // Hide the start screen
+  startScreen.style.display = "none"; // Hide the start screen
   enableCamera(); // Enable camera movement
 }
 
 // Load screen
 export function initializeLoadingScreen() {
-  const loadingScreen = document.createElement('div');
-  loadingScreen.id = 'loading-screen';
+  const loadingScreen = document.createElement("div");
+  loadingScreen.id = "loading-screen";
   loadingScreen.innerHTML = `
     <video autoplay muted loop>
       <source src="/MLG/dankloadscreen2.mp4" type="video/mp4">
@@ -77,11 +80,11 @@ export function initializeLoadingScreen() {
 }
 
 export function showLoadingScreen(loadingScreen) {
-  loadingScreen.style.display = 'flex';
+  loadingScreen.style.display = "flex";
   disableCamera(); // Ensure camera movement is disabled
 }
 
 export function hideLoadingScreen(loadingScreen) {
-  loadingScreen.style.display = 'none';
+  loadingScreen.style.display = "none";
   enableCamera(); // Re-enable camera movement
 }
